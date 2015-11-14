f0c4c4159875aec36d4a93b007bfb2e9dec32c17
ankurmangal <mangal.ankur.ankur8@gmail.com>
2015-11-13 22:01:54 -0500

    Implementing the mainActivity class, adding the algoritham for calculations in calculations class

--- app/src/main/java/com/myapplication/scientificcalculator/Calculations.java
+++ app/src/main/java/com/myapplication/scientificcalculator/Calculations.java
@@ -1,7 +1,92 @@
 package com.myapplication.scientificcalculator;
 
+import android.app.Activity;
+
 /**
  * Created by ankur on 2015-11-12.
  */
-public class Calculations {
+public class Calculations extends Activity{
+
+
+    private double inputtedNumber; //private double mOperand;
+    private String waitingOperator;//  private String mWaitingOperator;
+    private double mWaitingInputtedNumber;//private double mWaitingOperand;
+    private double mCalculatorMemory;//     private double mCalculatorMemory;
+
+
+    public static final String ADDITION = "+";
+    public static final String MULTIPLICATION = "*";
+    public static final String DIVIDE = "/";
+    public static final String SUBTRACTION = "-";
+    public static final String PLUSMINUS = "+/-";
+    public static final String CLEAR = "C";
+    public static final String CHANGE = "CHANGE";
+
+
+    //constructor
+    public Calculations() {
+        // initialize variables upon start
+        inputtedNumber = 0;
+        waitingOperator = "";
+        mWaitingInputtedNumber = 0;
+        mCalculatorMemory= 0;
+
+    }
+
+
+    public void setInputtedNumber(double inputNumber){
+
+        inputtedNumber = inputNumber;
+    }
+
+    public double getResult(){
+        return inputtedNumber;
+
+    }
+    // used on screen orientation change
+    public void setMemory(double calculatorMemory)
+    {
+        mCalculatorMemory = calculatorMemory;
+    }
+
+    // used on screen orientation change
+    public double getMemory()
+    {
+        return mCalculatorMemory;
+    }
+
+    public String toString() {
+        return Double.toString(inputtedNumber);
+    }
+
+    protected double performCalculation(String operator){
+
+        if(operator.equals(CLEAR)){
+            inputtedNumber = 0;
+            waitingOperator = "";
+            mWaitingInputtedNumber = 0
+            ;mCalculatorMemory = 0;
+        }
+        if (waitingOperator.equals(ADDITION)) {
+
+            inputtedNumber = mWaitingInputtedNumber + inputtedNumber;
+
+        }
+        else if(waitingOperator.equals(SUBTRACTION)){
+            inputtedNumber = mWaitingInputtedNumber - inputtedNumber;
+
+        }
+        else if(waitingOperator.equals(DIVIDE)){
+            inputtedNumber = mWaitingInputtedNumber / inputtedNumber ;
+
+        }
+
+        else if(waitingOperator.equals(MULTIPLICATION)){
+            inputtedNumber = mWaitingInputtedNumber * inputtedNumber;
+
+        }
+
+        return inputtedNumber;
+    }
+
 }
--- app/src/main/java/com/myapplication/scientificcalculator/MainActivity.java
+++ app/src/main/java/com/myapplication/scientificcalculator/MainActivity.java
@@ -1,37 +1,40 @@
 package com.myapplication.scientificcalculator;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
-import android.os.Bundle;
-import android.text.TextUtils;
 
 import java.text.DecimalFormat;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
 
+
     private TextView inputDisplay;
-    private boolean isInProcess = true;
+    private boolean isInProcess = false;
     /*private CalculatorBrain mCalculatorBrain;*/
     private static final String NUMBERS = ".0123456789";
+    private Calculations mCalculations ;
 
-    DecimalFormat df = new DecimalFormat("@######");
+    DecimalFormat df = new DecimalFormat("@##########");
 
-
-
-
+    @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+                requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
+
+        mCalculations = new Calculations();
         inputDisplay = (TextView) findViewById(R.id.textView1);
+
 
         df.setMaximumFractionDigits(0);
         df.setMinimumIntegerDigits(1);
@@ -56,40 +59,70 @@ public class MainActivity extends Activity implements View.OnClickListener {
         findViewById(R.id.buttonSubtract).setOnClickListener(this);
         findViewById(R.id.plusMinus).setOnClickListener(this);
         findViewById(R.id.scientific).setOnClickListener(this);
+        findViewById(R.id.buttonClear).setOnClickListener(this);
 
     }
 
+
     @Override
-    public void onClick (View v) {
+    public void onClick(View v) {
+
 
         String buttonPress = ((Button)v).getText().toString();
-
 
         //if button pressed is change
         //calculator changes to scientific calculator
         if(buttonPress.equalsIgnoreCase("change")){
-
             Intent intent = new Intent(MainActivity.this, MainActivity2.class);
             startActivity(intent);
         }
 
-        //proceeding if the button pressed containg any number form (.,0,1,2,3,4,5,6,7,8,9)
-        if (NUMBERS.contains(buttonPress)){
+        if (NUMBERS.contains(buttonPress)) {
 
-            //user is still wrtitng
-            if(isInProcess == true){
-                if(buttonPress.equals(".")){
-                    inputDisplay.setText(0 +buttonPress);
+            // number was pressed
+            if (isInProcess) {
+
+                if (buttonPress.equals(".")){
+                    if(inputDisplay.getText().toString().contains(".")){
+                    }} else {
+                    inputDisplay.append(buttonPress);
+                    }
+               } else {
+               if (buttonPress.equals(".")) {
+                    inputDisplay.setText(0 + buttonPress);
+                    } else {
+                   inputDisplay.setText(buttonPress);
                 }
-                else{inputDisplay.setText(buttonPress);
-
-                }
-
+               isInProcess = true;
             }
-
+            } else {
+            // operation was pressed
+            if (isInProcess) {
+                mCalculations.setInputtedNumber(Double.parseDouble(inputDisplay.getText().toString()));
+                isInProcess = false;
+                }
+            mCalculations.performCalculation(buttonPress);
+           inputDisplay.setText(df.format(mCalculations.getResult()));
+           }
         }
 
-    }
+    @Override
+   protected void onSaveInstanceState(Bundle outState) {
+       super.onSaveInstanceState(outState);
+        // Save variables on screen orientation change
+       outState.putDouble("OPERAND", mCalculations.getResult());
+        outState.putDouble("MEMORY", mCalculations.getMemory());
+       }
 
+    @Override
+   protected void onRestoreInstanceState(Bundle savedInstanceState) {
+       super.onRestoreInstanceState(savedInstanceState);
+       // Restore variables on screen orientation change
+        mCalculations.setInputtedNumber(savedInstanceState.getDouble("OPERAND"));
+        mCalculations.setMemory(savedInstanceState.getDouble("MEMORY"));
+        inputDisplay.setText(df.format(mCalculations.getResult()));
+        }
 
 }
+
+
